5dff63f58baeda8bbfd25fed7ce96ab203834108
Kiril Vatev <vatev.1@gmail.com>
2019-08-04 10:18:56 -0400

    adding cwd option, exposing absolute paths

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
+const path = require('path');
 const EventEmitter = require('events');
 const fs = require('fs');
 const globby = require('globby');
 
-module.exports = (pattern, { persistent = true } = {}) => {
+module.exports = (pattern, {
+  cwd = process.cwd(),
+  persistent = true
+} = {}) => {
   const events = new EventEmitter();
   const dirs = {};
   const files = {};
 
   const watch = (file, func) => fs.watch(file, { persistent }, func);
+  globby.stream(pattern, {
+    onlyFiles: false,
+    markDirectories: true,
+    cwd
+  }).on('data', file => {
+    const abspath = path.resolve(cwd, file);
 
-  globby.stream(pattern, { onlyFiles: false, markDirectories: true })
-    .on('data', file => {
-      if (/\/$/.test(file)) {
-        dirs[file] = watch(file, (type, name) => {
-          events.emit('change', {
-            path: file,
-            type, name,
-            entity: 'directory'
-          });
+    if (/\/$/.test(file)) {
+      dirs[abspath] = watch(file, (type, name) => {
+        events.emit('change', {
+          path: abspath,
+          type, name,
+          entity: 'directory'
         });
-      } else {
-        files[file] = watch(file, (type, name) => {
-          events.emit('change', {
-            path: file,
-            type, name,
-            entity: 'file'
-          });
-        });
-      }
-    })
-    .on('end', () => {
-      events.emit('ready', {
-        files: Object.keys(files),
-        dirs: Object.keys(dirs)
       });
-    })
-    .on('error', err => {
-      events.emit('error', err);
+      events.emit('addDir', { path: file });
+    } else {
+      files[abspath] = watch(file, (type, name) => {
+        events.emit('change', {
+          path: abspath,
+          type, name,
+          entity: 'file'
+        });
+      });
+      events.emit('add', { path: file });
+    }
+  }).on('end', () => {
+    events.emit('ready', {
+      files: Object.keys(files),
+      dirs: Object.keys(dirs)
     });
+  }).on('error', err => {
+    events.emit('error', err);
+  });
 
   return events;
 };
