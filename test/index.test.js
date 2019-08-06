89cac75b2449cd4b1ceb15dcd516933cf6db9baa
Kiril Vatev <vatev.1@gmail.com>
2019-08-05 23:01:50 -0400

    implementing ready/glob test

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,60 @@
+const path = require('path');
+const fs = require('fs-extra');
+const root = require('rootrequire');
+const { expect } = require('chai');
+
+const watchboy = require(root);
+
 /* eslint-env mocha */
 describe('watchboy', () => {
-  it('start watching expected files and directories');
+  const temp = path.resolve(root, 'temp');
+  const file = relpath => path.resolve(temp, relpath);
+  let watcher;
 
-  it('emits "ready" when all initial files are found');
+  beforeEach(async () => {
+    await fs.remove(temp);
+    await Promise.all([
+      file('one.txt'),
+      file('bananas/two.txt'),
+      file('bananas/three.txt'),
+      file('oranges/four.txt'),
+      file('oranges/five.txt'),
+      file('pineapples/six.txt'),
+    ].map(f => fs.outputFile(f, Math.random().toString(36))));
+  });
+  afterEach(async () => {
+    if (watcher) {
+      watcher.close();
+    }
+    await fs.remove(temp);
+  });
+
+  it('watches expected files and directories after "ready" event', async () => {
+    const dirs = [], files = [];
+
+    await new Promise(r => {
+      watcher = watchboy('**/*', { cwd: temp, persistent: false })
+        .on('add', ({ path }) => files.push(path))
+        .on('addDir', ({ path }) => dirs.push(path))
+        .on('ready', () => r());
+    });
+
+    expect(dirs.sort()).to.deep.equal([
+      path.resolve(temp),
+      path.resolve(temp, 'bananas'),
+      path.resolve(temp, 'oranges'),
+      path.resolve(temp, 'pineapples'),
+    ].sort());
+
+    expect(files.sort()).to.deep.equal([
+      file('one.txt'),
+      file('bananas/two.txt'),
+      file('bananas/three.txt'),
+      file('oranges/four.txt'),
+      file('oranges/five.txt'),
+      file('pineapples/six.txt')
+    ].sort());
+  });
 
   it('emits "add" when a new file is added');
 
@@ -17,4 +69,8 @@ describe('watchboy', () => {
   it('emits "add" when a new file is created inside a watched directory');
 
   it('emits "addDir" when a new directory is created inside a watched directory');
+
+  describe('close', () => {
+    it('stops all listeners');
+  });
 });
