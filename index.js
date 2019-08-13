b6362598e63f0838a5d3ba66a90805c84e04a0ec
Kiril Vatev <vatev.1@gmail.com>
2019-08-12 20:07:38 -0400

    temp: disabling mtime check

--- index.js
+++ index.js
@@ -192,11 +192,13 @@ module.exports = (pattern, {
           return void events.emit('unlink', evarg);
         }
 
+        console.log(abspath, files[abspath]._wb_modified, stat.mtime.toISOString());
+
         // the file actually changed
-        if (files[abspath]._wb_modified < stat.mtime.toISOString()) {
-          files[abspath]._wb_modified = stat.mtime.toISOString();
+//        if (files[abspath]._wb_modified < stat.mtime.toISOString()) {
+//          files[abspath]._wb_modified = stat.mtime.toISOString();
           return void events.emit('change', evarg);
-        }
+//        }
       }).catch(err => {
         error(err, abspath);
       });
